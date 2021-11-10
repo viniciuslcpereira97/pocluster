3d3100e39be2018a2262fc8366568910469c4403
Vinicius Luiz <viniciuslcp97@gmail.com>
2021-11-10 18:17:00 -0300

    add function to search ets cache in all nodes

--- apps/pocluster/lib/pocluster.ex
+++ apps/pocluster/lib/pocluster.ex
@@ -6,4 +6,17 @@ defmodule Pocluster do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  alias Pocluster.CacheClient
+
+  def get_cache(key) do
+    CacheClient.get(key)
+  end
+
+  def get_cache_in_all_nodes(key) do
+    Node.list()
+    |> Enum.map(fn node ->
+      :rpc.call(node, Pocluster, :get_cache, [key])
+    end)
+  end
 end
--- apps/pocluster/lib/pocluster/application.ex
+++ apps/pocluster/lib/pocluster/application.ex
@@ -5,9 +5,16 @@ defmodule Pocluster.Application do
 
   @impl true
   def start(_type, _args) do
+    topologies = [
+      jobs: [
+        strategy: Cluster.Strategy.Gossip
+      ]
+    ]
+
     children = [
       {Phoenix.PubSub, name: Pocluster.PubSub},
-      {Pocluster.CacheServer, name: Pocluster.CacheServer}
+      {Pocluster.CacheServer, name: Pocluster.CacheServer},
+      {Cluster.Supervisor, [topologies, [name: Pocluster.ClusterSupervisor]]}
     ]
 
     Supervisor.start_link(children, strategy: :one_for_one, name: Pocluster.Supervisor)
